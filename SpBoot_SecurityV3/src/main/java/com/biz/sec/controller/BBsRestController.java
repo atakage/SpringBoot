b27eb8e10d7f6d8a908c82c388ce1df1fffc1a31
atakage <atakoko2@gmail.com>
2020-05-08 16:50:51 +0900

    202005081650

--- SpBoot_SecurityV3/src/main/java/com/biz/sec/controller/BBsRestController.java
+++ SpBoot_SecurityV3/src/main/java/com/biz/sec/controller/BBsRestController.java
@@ -1,10 +1,13 @@
 package com.biz.sec.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.biz.sec.domain.BBsVO;
@@ -37,18 +40,50 @@ public class BBsRestController {
 	}
 	
 	
-	@RequestMapping(value="/insert", method=RequestMethod.POST)
-	@CrossOrigin(origins ="http://localhost:3000")
-	public String insert(BBsVO bbsVO) {
 	
-			log.debug(bbsVO.toString());
-			
-			bService.insert(bbsVO);
+	
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	@CrossOrigin(origins ="http://localhost:3000")
+	public BBsVO getDetail(@RequestParam("bbsid") String bbsid){
 		
-		return "ok";
+		//List<BBsVO> bbsList = bService.getBbsList();
+		
+		log.debug("게시판아이디: " + bbsid);
+		
+		long id=0;
+		
+		try {
+		
+			id = Long.valueOf(bbsid);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	
+		Optional<BBsVO> optBBsVO = bService.findById(id);
+		
+		log.debug("ddddd"+ optBBsVO.get().toString());
+		
+		return optBBsVO.get();
 		
 	}
 	
 	
+	
+	@RequestMapping(value="/insert", method=RequestMethod.POST)
+	@CrossOrigin(origins ="http://localhost:3000")
+	public BBsVO insert(BBsVO bbsVO) {
+	
+			log.debug(bbsVO.toString());
+			
+			bbsVO = bService.insert(bbsVO);
+		
+		return bbsVO;
+		
+	}
+	
+
 	
 }
--- SpBoot_SecurityV3/src/main/java/com/biz/sec/service/BBsService.java
+++ SpBoot_SecurityV3/src/main/java/com/biz/sec/service/BBsService.java
@@ -1,6 +1,7 @@
 package com.biz.sec.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -62,6 +63,17 @@ public class BBsService {
 	public BBsVO insert(BBsVO bbsVO) {
 		// TODO Auto-generated method stub
 		return bDao.save(bbsVO);
+	}
+
+
+
+	public Optional<BBsVO> findById(long id) {
+		// TODO Auto-generated method stub
+		
+		Optional<BBsVO> bbsVO = bDao.findById(id);
+
+		
+		return bbsVO;
 	}
 	
 	
